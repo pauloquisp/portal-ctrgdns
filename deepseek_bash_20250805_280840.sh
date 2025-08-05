43bafd19ac51b026b19a333dfcd1bbbc190f092a
pauloquispe <136033186+pauloquisp@users.noreply.github.com>
2025-08-04 21:13:22 -0400

    Primera versión del portal

---
+++ deepseek_bash_20250805_280840.sh
@@ -0,0 +1,7 @@
+cd ruta/a/tu/carpeta  # Ejemplo: cd Documents/mi-proyecto
+git init
+git add .
+git commit -m "Primer commit"
+git branch -M main
+git remote add origin https://github.com/tuusuario/portal-gedeones.git
+git push -u origin main
